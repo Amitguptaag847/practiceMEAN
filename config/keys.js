ae425d09b6ace33d4a3410bdccf1433bba7162db
Amit <amitgupta.ag847@gmail.com>
2020-08-16 20:50:55 +0530

    final commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://AmitGupta:Amit12345@cluster0-txk29.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: 'MongoDB url',
     secretOrKey: 'secret'
 }
